9c83b4f6b1a7a95bc9408fec2d3c695d40403c4f
Kelly Johnson <kellyfishbackjohnson@gmail.com>
2016-10-07 21:19:42 -0400

    Good attempt

--- FireSwamp/src/CITtwosixtyFireSwamp/LightningSandsTypeScene.java
+++ FireSwamp/src/CITtwosixtyFireSwamp/LightningSandsTypeScene.java
@@ -5,10 +5,98 @@
  */
 package CITtwosixtyFireSwamp;
 
+import java.io.Serializable;
+import java.util.Objects;
 /**
  *
  * @author kellyjohnson
  */
-public class LightningSandsTypeScene {
+public class LightningSandsTypeScene implements Serializable{
+    
+    // class instance variables
+    private String lighteningFormula;
+    private String outcomeTrue;
+    private String outcomeFalse;
+    private String attribute;
+
+    public LightningSandsTypeScene() {
+    }
+
+    
+    public String getLighteningFormula() {
+        return lighteningFormula;
+    }
+
+    public void setLighteningFormula(String lighteningFormula) {
+        this.lighteningFormula = lighteningFormula;
+    }
+
+    public String getOutcomeTrue() {
+        return outcomeTrue;
+    }
+
+    public void setOutcomeTrue(String outcomeTrue) {
+        this.outcomeTrue = outcomeTrue;
+    }
+
+    public String getOutcomeFalse() {
+        return outcomeFalse;
+    }
+
+    public void setOutcomeFalse(String outcomeFalse) {
+        this.outcomeFalse = outcomeFalse;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.lighteningFormula);
+        hash = 67 * hash + Objects.hashCode(this.outcomeTrue);
+        hash = 67 * hash + Objects.hashCode(this.outcomeFalse);
+        hash = 67 * hash + Objects.hashCode(this.attribute);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "LightningSandsTypeScene{" + "lightening=" + lighteningFormula + ", outcomeTrue=" + outcomeTrue + ", outcomeFalse=" + outcomeFalse + ", attribute=" + attribute + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LightningSandsTypeScene other = (LightningSandsTypeScene) obj;
+        if (!Objects.equals(this.lighteningFormula, other.lighteningFormula)) {
+            return false;
+        }
+        if (!Objects.equals(this.outcomeTrue, other.outcomeTrue)) {
+            return false;
+        }
+        if (!Objects.equals(this.outcomeFalse, other.outcomeFalse)) {
+            return false;
+        }
+        if (!Objects.equals(this.attribute, other.attribute)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
