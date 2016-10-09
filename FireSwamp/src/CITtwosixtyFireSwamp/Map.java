a519ed8875dd2ae1df6b3c28485c52f9490ddcf3
dayanacarranza <dayanacarranza@10.50.100.238>
2016-10-08 20:07:55 -0600

    10/07/2016

--- FireSwamp/src/CITtwosixtyFireSwamp/Map.java
+++ FireSwamp/src/CITtwosixtyFireSwamp/Map.java
@@ -1,74 +1,1 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package CITtwosixtyFireSwamp;
-
-import java.io.Serializable;
-import java.util.Objects;
-
-/**
- *
- * @author dayanacarranza
- */
-public class Map implements Serializable{
-    
-    private Double rowCount;
-    private Double columnCount;
-
-    public Map() {
-    }
-
-    public Double getRowCount() {
-        return rowCount;
-    }
-
-    public Double getColumnCount() {
-        return columnCount;
-    }
-
-    public void setRowCount(Double rowCount) {
-        this.rowCount = rowCount;
-    }
-
-    public void setColumnCount(Double columnCount) {
-        this.columnCount = columnCount;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 79 * hash + Objects.hashCode(this.rowCount);
-        hash = 79 * hash + Objects.hashCode(this.columnCount);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Map other = (Map) obj;
-        if (!Objects.equals(this.rowCount, other.rowCount)) {
-            return false;
-        }
-        if (!Objects.equals(this.columnCount, other.columnCount)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + '}';
-    }
-    
-    
-}
+/* * To change this license header, choose License Headers in Project Properties. * To change this template file, choose Tools | Templates * and open the template in the editor. */package CITtwosixtyFireSwamp;import java.io.Serializable;import java.util.Objects;/** * * @author dayanacarranza */public class Map implements Serializable{        private Double rowCount;    private Double columnCount;    public Map() {    }    public Double getRowCount() {        return rowCount;    }    public Double getColumnCount() {        return columnCount;    }    public void setRowCount(Double rowCount) {        this.rowCount = rowCount;    }    public void setColumnCount(Double columnCount) {        this.columnCount = columnCount;    }    @Override    public int hashCode() {        int hash = 5;        hash = 79 * hash + Objects.hashCode(this.rowCount);        hash = 79 * hash + Objects.hashCode(this.columnCount);        return hash;    }    @Override    public boolean equals(Object obj) {        if (this == obj) {            return true;        }        if (obj == null) {            return false;        }        if (getClass() != obj.getClass()) {            return false;        }        final Map other = (Map) obj;        if (!Objects.equals(this.rowCount, other.rowCount)) {            return false;        }        if (!Objects.equals(this.columnCount, other.columnCount)) {            return false;        }        return true;    }    @Override    public String toString() {        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + '}';    }        }
